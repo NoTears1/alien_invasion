65b04d9ba3dd7aceda1a93afc3e4120d7b94fb8f
NoTears <786558291@qq.com>
2023-11-29 16:50:48 +0800

    ''Nov. 29th develop alien_invasion"

--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ class Settings():
     def __init__(self):
         '''初始化游戏的设置'''
         # 屏幕设置
-        self.screen_witdth = 1200
-        self.screen_height = 600
+        self.screen_width = 1200
+        self.screen_height = 650
         self.bg_color = (230, 230, 230)
 
         # 飞船设置
@@ -13,6 +13,13 @@ class Settings():
 
         # 子弹设置
         self.bullet_speed_factor = 1
-        self.bullet_width = 3
-        self.bullet_height = 15
+        self.bullet_width = 15
+        self.bullet_height = 3
         self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
+        # 外星人设置
+        self.alien_speed_factor = 0.5
+        self.fleet_right_speed = 10
+        # fleet_direction为1表示向下移，为-1表示向上移
+        self.fleet_direction = 1
